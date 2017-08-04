13a6692bb623987f6891a94119efcaf9ff386bcd
Greg Maslowski <gregmaslowski@gmail.com>
2017-08-04 11:01:59 +0200

    Added basic watch possibility.

--- addon.py
+++ addon.py
@@ -2,6 +2,7 @@ import sys
 import urllib
 import urlparse
 
+import xbmc
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
@@ -23,9 +24,37 @@ def build_url(params):
 action = ADDON_ARGS.get('action', None)
 
 if action is None:
-    for item in wizjatv.list_channels():
-        li = xbmcgui.ListItem(item['title'], iconImage=item['thumb'], thumbnailImage=item['thumb'])
+
+    channels = wizjatv.list_channels()
+    for channel in channels:
+        li = xbmcgui.ListItem(channel['title'], iconImage=channel['thumb'], thumbnailImage=channel['thumb'])
         li.setProperty('IsPlayable', 'true')
-        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url="", listitem=li)
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=build_url({'action': 'play', 'channel': channel['id']}),
+                                    listitem=li, totalItems=len(channels))
     xbmcplugin.addSortMethod(ADDON_HANDLE, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     xbmcplugin.endOfDirectory(ADDON_HANDLE)
+
+elif action[0] == 'play':
+    if ADDON.getSetting('plugin.video.kodiver.wizjatv.user') == '':
+        ok = xbmcgui.Dialog().ok("No user/pass set for WizjaTV",
+                                 "You'll be redirected to settings page to set user/pass", "for WizjaTV.")
+        ADDON.openSettings()
+
+    url = wizjatv.channelStream(ADDON_ARGS.get('channel', None)[0])
+
+    xbmc.log(url, xbmc.LOGWARNING)
+
+    try:
+        playItem = xbmcgui.ListItem('title', path=url)
+        playItem.setInfo(type="video", infoLabels={"Title": "Title"})
+
+        xbmcPlayer = xbmc.Player()
+        xbmcplugin.setResolvedUrl(ADDON_HANDLE, True, playItem)
+
+        for i in range(0, 240):
+            if xbmcPlayer.isPlayingVideo():
+                break
+            xbmc.sleep(1000)
+
+    except Exception as e:
+        xbmc.log('ERROR PLAY:%s' % e, xbmc.LOGWARNING)
--- wizjatv.py
+++ wizjatv.py
@@ -1,9 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import re
+import urllib
+
 import requests
 import xbmc
-
+import xbmcaddon
 from bs4 import BeautifulSoup
 
 WIZJA_TV_URL = 'https://wizja.tv/'
+WIZJA_TV_LOGIN_URL = WIZJA_TV_URL + 'users/index.php'
+WIZJA_TV_WATCH_URL = WIZJA_TV_URL + 'watch.php'
+WIZJA_TV_PORTER_URL = WIZJA_TV_URL + 'porter.php'
+WIZJA_TV_KILLME_URL = WIZJA_TV_URL + 'killme.php'
 
 
 def list_channels():
@@ -12,17 +21,80 @@ def list_channels():
     r.encoding = 'utf-8'
     page = BeautifulSoup(r.text, 'html.parser')
     html_channels = page.find_all('div')[1]
-    xbmc.log(str(html_channels), xbmc.LOGERROR)
 
     for channel in html_channels.find_all("a"):
-        xbmc.log(str(channel), xbmc.LOGERROR)
         image_source = str(channel.find_all('img')[0]['src'])
+        channel_id = str(channel['href']).replace('watch.php?id=', '')
+
         channels.append({
-            'id': str(channel['href']).replace('watch.php?id=', ''),
+            'id': channel_id,
             'thumb': WIZJA_TV_URL + image_source,
             'title': image_source.replace('ch_logo/', '').replace('.png', '').upper()
         })
 
-    xbmc.log(str(channels), xbmc.LOGERROR)
-
     return channels
+
+
+def wizjalogin():
+    try:
+        params = {}
+        url = WIZJA_TV_LOGIN_URL
+
+        params['login'] = 'zaloguj'
+        params['user_name'] = xbmcaddon.Addon(id='plugin.video.kodiver.wizjatv').getSetting(
+            'plugin.video.kodiver.wizjatv.user')
+        params['user_password'] = xbmcaddon.Addon(id='plugin.video.kodiver.wizjatv').getSetting(
+            'plugin.video.kodiver.wizjatv.pass')
+
+        result = requests.post(url, data=params)
+
+        cookie = result.cookies.get_dict()
+
+        xbmcaddon.Addon(id='plugin.video.kodiver.wizjatv').setSetting('plugin.video.kodiver.wizjatv.cookie',
+                                                                      str(cookie))
+
+    except Exception as e:
+        xbmc.log(e, xbmc.LOGWARNING)
+    return
+
+
+def channelStream(channel_id):
+    wizjalogin()
+
+    try:
+
+        cookie = eval(
+            xbmcaddon.Addon(id='plugin.video.kodiver.wizjatv').getSetting('plugin.video.kodiver.wizjatv.cookie'))
+
+        ref = WIZJA_TV_WATCH_URL + '?id=%s' % channel_id
+        requests.get(ref, cookies=cookie)
+        url = WIZJA_TV_PORTER_URL + '?ch=%s' % channel_id
+
+        result = requests.get(url, cookies=cookie, headers={'Referer': ref})
+        content = result.content
+
+        if '<a href="killme.php?id' in content:
+            requests.get(WIZJA_TV_KILLME_URL + '?id=%s' % channel_id, cookies=cookie, headers={'Referer': ref})
+
+            ref = WIZJA_TV_WATCH_URL + '?id=%s' % channel_id
+            requests.get(ref, cookies=cookie, headers={'Referer': ref})
+            result = requests.get(url, cookies=cookie, headers={'Referer': ref})
+            content = result.content
+
+        return createRtmpFromSrc(re.compile('src: "(.*?)"').findall(content)[0], ref)
+
+    except Exception as e:
+        xbmc.log(e, xbmc.LOGWARNING)
+    return
+
+
+def createRtmpFromSrc(src, ref):
+    decodedRtmp = urllib.unquote().decode('utf8')
+    rtmpSections = re.compile('rtmp://(.*?)/(.*?)/(.*?)\?(.*?)\&streamType').findall(decodedRtmp)
+    xbmcRtmp = 'rtmp://' + rtmpSections[0][0] + '/' + rtmpSections[0][1] + '?' + rtmpSections[0][3] + \
+               ' app=' + rtmpSections[0][1] + '?' + rtmpSections[0][3] + \
+               ' playpath=' + rtmpSections[0][2] + '?' + rtmpSections[0][3] + \
+               ' swfVfy=1 flashver=LNX\\25,0,0,12 timeout=25 ' \
+               'swfUrl=https://wizja.tv/player/StrobeMediaPlayback_v4.swf live=true ' \
+               'pageUrl=' + ref
+    return xbmcRtmp
